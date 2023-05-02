d14bf75b6f1c624992f153fbbf3050c8a72db153
Kamil <mentos.96@o2.pl>
2023-05-02 22:13:22 +0200

    Adding mouse click and behaviour of icon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,7 +29,9 @@ namespace MatchGame
 
         private void SetUpGame()
         {
-            List<String> animalEmoji = new List<String>()
+
+            
+            List<String> animalEmoji = new()
             {
                 "🐙","🐙",
                 "🐡","🐡",
@@ -47,6 +49,31 @@ namespace MatchGame
                 String nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
+            }
+        }
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
             }
         }
     }
